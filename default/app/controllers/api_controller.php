33a7c706d7c1f8623cfffcc336dbf0785508c71f
murillojulio <juarmuri@gmail.com>
2020-06-24 23:10:08 -0500

    Se indento el codigo

--- default/app/controllers/api_controller.php
+++ default/app/controllers/api_controller.php
@@ -10,105 +10,102 @@ class ApiController extends RestController
     public function get($departamento_id)
     {
         $territorio = new Territorio();
-        $array = array('records'=>$territorio->find_all_by('departamento_id', $departamento_id));
+        $array = array('records' => $territorio->find_all_by('departamento_id', $departamento_id));
         $this->data = $array;
     }
-	
-	public function get_departamentos()
+
+    public function get_departamentos()
     {
         $departamento = new Departamento();
-        $array = array('departamentos'=>$departamento->find_all_by_sql('SELECT departamento.id, departamento.nombre, departamento.area_total, departamento.capital, departamento.cant_municipio_pacifico  FROM departamento WHERE departamento.id !=0 ORDER BY departamento.nombre ASC'));
+        $array = array('departamentos' => $departamento->find_all_by_sql('SELECT departamento.id, departamento.nombre, departamento.area_total, departamento.capital, departamento.cant_municipio_pacifico  FROM departamento WHERE departamento.id !=0 ORDER BY departamento.nombre ASC'));
         $this->data = $array;
     }
-	
-	public function get_municipios($departamento_id)
+
+    public function get_municipios($departamento_id)
     {
         $municipio = new Municipio();
-        $array = array('municipios'=>$municipio->find_all_by_sql("SELECT municipio.id, municipio.nombre, municipio.area_total,  municipio.area_rural, municipio.cabecera, municipio.area_cabecera, municipio.certificado, municipio.fecha_creacion FROM municipio WHERE municipio.departamento_id=$departamento_id ORDER BY municipio.nombre ASC"));
+        $array = array('municipios' => $municipio->find_all_by_sql("SELECT municipio.id, municipio.nombre, municipio.area_total,  municipio.area_rural, municipio.cabecera, municipio.area_cabecera, municipio.certificado, municipio.fecha_creacion FROM municipio WHERE municipio.departamento_id=$departamento_id ORDER BY municipio.nombre ASC"));
         $this->data = $array;
     }
-	
-	public function get_territorios($municipio_id) { 
+
+    public function get_territorios($municipio_id)
+    {
         $territorios = new TerritorioMunicipio();
-       /* 
+        /* 
 	   $array = array('territorios'=>$territorios->find_all_by_sql("SELECT municipio.nombre AS municipio_nombre, territorio.*, departamento.nombre AS departamento_nombre FROM territorio_municipio INNER JOIN territorio ON territorio.id = territorio_municipio.territorio_id INNER JOIN municipio ON municipio.id = territorio_municipio.municipio_id INNER JOIN departamento ON departamento.id = territorio.departamento_id WHERE territorio_municipio.municipio_id = $municipio_id GROUP BY territorio.nombre ORDER BY nombre ASC")); 
 	   */
-		$array = array('territorios'=>$territorios->find_all_by_sql("SELECT territorio.id, territorio.nombre, territorio.tipo FROM territorio_municipio INNER JOIN territorio ON territorio.id = territorio_municipio.territorio_id INNER JOIN municipio ON municipio.id = territorio_municipio.municipio_id WHERE territorio_municipio.municipio_id = $municipio_id ORDER BY nombre ASC")); 
-		$this->data = $array;        
+        $array = array('territorios' => $territorios->find_all_by_sql("SELECT territorio.id, territorio.nombre, territorio.tipo FROM territorio_municipio INNER JOIN territorio ON territorio.id = territorio_municipio.territorio_id INNER JOIN municipio ON municipio.id = territorio_municipio.municipio_id WHERE territorio_municipio.municipio_id = $municipio_id ORDER BY nombre ASC"));
+        $this->data = $array;
     }
 
 
-	public function get_territorio($id) {
-		 $obj_territorio = new Territorio();        
-         $obj_territorio->getTerritorioById($id); 
-         $array_territorio = $obj_territorio;
-        
+    public function get_territorio($id)
+    {
+        $obj_territorio = new Territorio();
+        $obj_territorio->getTerritorioById($id);
+        $array_territorio = $obj_territorio;
+
         $obj_territorio_municipio = new TerritorioMunicipio();
         $array_ubicaciones = $obj_territorio_municipio->getDepartamentoMunicipioByTerritorioId($id);
         $array_titulado = array();
-		
-        if ($array_territorio->titulado == 'SI')
-        {
+
+        if ($array_territorio->titulado == 'SI') {
             $obj_titulado_si = new TituladoSi();
             $obj_titulado_si->getTituladoSiByTerritorioId($id);
             $array_titulado = $obj_titulado_si;
         }
-       
-        if ($obj_territorio->titulado == 'NO')
-        {
+
+        if ($obj_territorio->titulado == 'NO') {
             $obj_titulado_no = new TituladoNo();
             $obj_titulado_no->getTituladoNoByTerritorioId($id);
             $array_titulado = $obj_titulado_no;
         }
-        
+
         $poblacion = new Poblacion();
-        $poblacion = Poblacion::getPoblacion('territorio_id', $id);        
+        $poblacion = Poblacion::getPoblacion('territorio_id', $id);
         $array_poblacion = $poblacion;
-        
+
         $obj_comunidades = new Comunidad();
         $array_comunidades = $obj_comunidades->getComunidadesByTerritorioId($id);
-        
+
         $cantidad_comunidad = 0;
-        foreach($this->$array_comunidades as $registros):
+        foreach ($this->$array_comunidades as $registros) :
             $cantidad_comunidad++;
-        endforeach;        
+        endforeach;
         $this->cantidad_comunidad = $cantidad_comunidad;
-        
+
         $obj_conflictos = new Conflicto();
         $array_conflictos = $obj_conflictos->getConflictosByTerritorioId($id);
-		
-		$array = array('informacion_basica'=>array($array_territorio,$array_titulado), 'ubicacion'=>$array_ubicaciones, 'poblacion'=>$array_poblacion, 'comunidades'=>$array_comunidades, 'conflictos'=>$array_conflictos);
-		$this->data = $array;
-	}
-	
-	public function get_territorio_informacion($id) {
-		 $obj_territorio = new Territorio();        
-         $obj_territorio->getTerritorioById($id); 
-         $array_territorio = $obj_territorio;
-        
+
+        $array = array('informacion_basica' => array($array_territorio, $array_titulado), 'ubicacion' => $array_ubicaciones, 'poblacion' => $array_poblacion, 'comunidades' => $array_comunidades, 'conflictos' => $array_conflictos);
+        $this->data = $array;
+    }
+
+    public function get_territorio_informacion($id)
+    {
+        $obj_territorio = new Territorio();
+        $obj_territorio->getTerritorioById($id);
+        $array_territorio = $obj_territorio;
+
         $obj_territorio_municipio = new TerritorioMunicipio();
         $array_ubicaciones = $obj_territorio_municipio->getDepartamentoMunicipioByTerritorioId($id);
         $array_titulado = array();
-		
-        if ($array_territorio->titulado == 'SI')
-        {
+
+        if ($array_territorio->titulado == 'SI') {
             $obj_titulado_si = new TituladoSi();
             $obj_titulado_si->getTituladoSiByTerritorioId($id);
             $array_titulado = $obj_titulado_si;
         }
-       
-        if ($obj_territorio->titulado == 'NO')
-        {
+
+        if ($obj_territorio->titulado == 'NO') {
             $obj_titulado_no = new TituladoNo();
             $obj_titulado_no->getTituladoNoByTerritorioId($id);
             $array_titulado = $obj_titulado_no;
         }
-        
-        
-		
-		$array = array('informacion_basica'=>array($array_territorio,$array_titulado));
-		$this->data = $array;
-	}
-}
 
-?>
+
+
+        $array = array('informacion_basica' => array($array_territorio, $array_titulado));
+        $this->data = $array;
+    }
+}
