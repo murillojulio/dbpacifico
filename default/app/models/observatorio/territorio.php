5222636396181307bf5f99a07cf120c824fb26d3
murillojulio <juarmuri@gmail.com>
2020-07-03 00:39:42 -0500

    Se implementa gestion territorial para los territorios urbanos

--- default/app/models/observatorio/territorio.php
+++ default/app/models/observatorio/territorio.php
@@ -111,7 +111,7 @@ class Territorio extends ActiveRecord {
             {
                 return $this->paginated_by_sql('SELECT territorio.*, departamento.nombre AS departamento,
                 (SELECT COUNT(consejo.id) FROM consejo WHERE consejo.territorio_id = territorio.id) AS cant_consejo_reg,
-                (SELECT COUNT(actor_armado.presunto_responsable_id) FROM actor_armado WHERE actor_armado.territorio_id = territorio.id) AS cant_actor_armado_reg,
+                /*(SELECT COUNT(actor_armado.presunto_responsable_id) FROM actor_armado WHERE actor_armado.territorio_id = territorio.id) AS cant_actor_armado_reg,*/
                 (SELECT COUNT(caso.id) FROM caso WHERE caso.territorio_id = territorio.id) AS cant_caso_reg FROM territorio 
                 INNER JOIN departamento ON departamento.id = territorio.departamento_id                
                 WHERE'.$condicion.'territorio.id IS NOT NULL AND tipo="'.$tipo.'" GROUP BY territorio.nombre ORDER BY '.$order, "page: $page");                 
@@ -120,7 +120,7 @@ class Territorio extends ActiveRecord {
             {
                 return $this->paginated_by_sql('SELECT territorio.*, departamento.nombre AS departamento,
                 (SELECT COUNT(cabildo.id) FROM cabildo WHERE cabildo.territorio_id = territorio.id) AS cant_cabildo_reg,
-                (SELECT COUNT(actor_armado.presunto_responsable_id) FROM actor_armado WHERE actor_armado.territorio_id = territorio.id) AS cant_actor_armado_reg,
+                /*(SELECT COUNT(actor_armado.presunto_responsable_id) FROM actor_armado WHERE actor_armado.territorio_id = territorio.id) AS cant_actor_armado_reg,*/
                 (SELECT COUNT(caso.id) FROM caso WHERE caso.territorio_id = territorio.id) AS cant_caso_reg FROM territorio 
                 INNER JOIN departamento ON departamento.id = territorio.departamento_id                
                 WHERE'.$condicion.'territorio.id IS NOT NULL AND tipo="'.$tipo.'" GROUP BY territorio.nombre ORDER BY '.$order, "page: $page");                 
@@ -129,7 +129,7 @@ class Territorio extends ActiveRecord {
             {
                 return $this->paginated_by_sql('SELECT territorio.*, departamento.nombre AS departamento,
                 (SELECT COUNT(consejo.id) FROM consejo WHERE consejo.territorio_id = territorio.id) AS cant_consejo_reg,
-                (SELECT COUNT(actor_armado.presunto_responsable_id) FROM actor_armado WHERE actor_armado.territorio_id = territorio.id) AS cant_actor_armado_reg,
+                /*(SELECT COUNT(actor_armado.presunto_responsable_id) FROM actor_armado WHERE actor_armado.territorio_id = territorio.id) AS cant_actor_armado_reg,*/
                 (SELECT COUNT(caso.id) FROM caso WHERE caso.territorio_id = territorio.id) AS cant_caso_reg FROM territorio 
                 INNER JOIN departamento ON departamento.id = territorio.departamento_id                
                 WHERE'.$condicion.'territorio.id IS NOT NULL AND tipo="'.$tipo.'" GROUP BY territorio.nombre ORDER BY '.$order, "page: $page");                 
